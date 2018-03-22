bab9221329bd2e4a133c3049428da3d992952e59
darke <darke7@163.com>
2018-03-23 00:59:20 +0800

    use handlebars

--- site/app.js
+++ site/app.js
@@ -1,11 +1,14 @@
 let express = require('express');
 let app = express();
+let handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 
 let home = require('./router/home.js');
 
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port',process.env.POERT||3000);
 
-app.use('', home);
+app.use('/', home);
 
 
 
@@ -13,15 +16,13 @@ app.use('', home);
 
 
 app.use((req,res)=>{
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 -- nofound');
+	res.render('404');
 });
 
 app.use((err,req,res,next)=>{
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 -- server error');
+	res.render('500');
 });
 
 app.listen(app.get('port'),(req,res)=>{
--- site/router/home.js
+++ site/router/home.js
@@ -1,16 +1,13 @@
 let express = require('express');
 let router = express.Router();
 
+
 router.get('/',(req,res)=>{
-	res.type('text/plain');
-	res.status(200);
-	res.send('welcome Eserver!');
+	res.render('home');
 });
 
 router.get('/about',(req,res)=>{
-	res.type('text/plain');
-	res.status(200);
-	res.send('about Eserver');
+	res.render('about');
 });
 
 module.exports = router;
